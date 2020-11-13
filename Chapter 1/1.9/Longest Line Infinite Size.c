263a6fb0283817886b461922bab86d9fec3e620a
alphaomega325 <alphaomega325@gmail.com>
2020-11-12 21:25:09 -0500

    Made the program to accept as much input as I can figure out to allow it to

--- Chapter 1/1.9/Longest Line Infinite Size.c
+++ Chapter 1/1.9/Longest Line Infinite Size.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
-#define MAXLINE 1000 /* maximum input line length */
+#include <stdlib.h>
+#include <limits.h>
 
-int self_implemented_getline(char line[]);
+void self_implemented_getline(char line[], int *len);
 void copy(char to[], char from[]);
-int array_element_num(char array[]);
 
 /* print the longest input line */
 int main(){
 
-  int len;               /* current line length */
+  int len = 0;               /* current line length */
   int max;               /* maximum length seens so far */
-  char line[MAXLINE];    /* current input line */
-  char longest[MAXLINE]; /* longest line saved here */
+  char *line;    /* current input line */
+  char *longest;      /* longest line saved here */
+  int *lenptr = &len;
 
+  line = malloc(INT_MAX * sizeof(char));
+  longest = malloc(INT_MAX * sizeof(char));
+  
   max = 0;
-  while ((len = self_implemented_getline(line)) > 0){
+  do{
+    self_implemented_getline(line, lenptr);
     if (len > max) {
       max = len;
       copy(longest, line);
     }
-  }
+  }while(len > 0);
 
   if (max > 0)  /* there was a line */
     printf("%s", longest);
@@ -30,29 +35,16 @@ int main(){
 
 
 /* getline: read a line into s, return length */
-int self_implemented_getline(char s[]){
+void self_implemented_getline(char s[], int *len){
 
   int c, i;
-  int lim = array_element_num(s);
   
-  for(i = 0; i < lim - 1 && (c=getchar()) != EOF && c != '\n'; ++i){
-    if(i == lim - 2){
-      char temp[lim + 100];
-      copy(temp, s);
-      s = temp;
-      lim = array_element_num(s);
-    }
+  for(i = 0;(c=getchar()) != EOF && c != '\n'; ++i){
     s[i] = c;
   }
-
-  if (c == '\n'){
-    s[i] = c;
-    ++i;
-  }
-
+  
   s[i] = '\0';
-  return i;
-
+  *len = i;
 
 }
 
@@ -60,21 +52,10 @@ int self_implemented_getline(char s[]){
 void copy(char to[], char from[]){
 
   int i = 0;
-  int lim = array_element_num(from);
-  if(array_element_num(to) < lim){
-    char temp[lim];
-    to = temp;
-  }
-  
-  while (i < lim - 1){
+  while (from[i] != '\0'){
     to[i] = from[i];
     ++i;
   }
 
 }
 
-/* array_element_num: shows how many elements are in a array */
-int array_element_num(char s[]){
-  return (int)(sizeof(s)/sizeof(s[0]));
-
-}
